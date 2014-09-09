94e4459fff613073d00e4420837c859aceb46044
tamswebdev <portaladmin@tams.com>
2014-09-09 11:04:23 -0700

    updated production configs

--- Scripts/environment-configs.js
+++ Scripts/environment-configs.js
@@ -1,6 +1,6 @@
 //This is a configuration file for all hard-coded values in a specific environment
 
 var Configs = {
-	"ServiceRootUrl": "http://tusspdev1/VirtualApps/BusOpsWebs/TAMS.BUSOPS.DemoESR.MobileSVC/",
-	"SharePointRootUrl" : "http://tusspdev1/"
+	"ServiceRootUrl": "https://intranet.tams.com/VirtualApps/TAMS.BUSOPS/BUSOPSWebs/TAMS.BUSOPS.DemoESR.MobileSVC/",
+	"SharePointRootUrl" : "https://intranet.tams.com/"
 };
