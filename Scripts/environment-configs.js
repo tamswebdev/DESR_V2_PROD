0f7861e108d351265afcadfbc20d7816bf9b47d1
tamsdomain\upasha <upasha@tams.com>
2015-10-22 14:31:05 -0700

    back to dev 2

--- Scripts/environment-configs.js
+++ Scripts/environment-configs.js
@@ -1,12 +1,12 @@
 //This is a configuration file for all hard-coded values in a specific environment
 
 var Configs = {
-	"ServiceRootUrl": "https://intranet.tams.com/VirtualApps/TAMS.BUSOPS/BUSOPSWebs/TAMS.BUSOPS.DemoESR.MobileSVC/",
-	"SharePointRootUrl" : "https://intranet.tams.com/"
+	//"ServiceRootUrl": "https://intranet.tams.com/VirtualApps/TAMS.BUSOPS/BUSOPSWebs/TAMS.BUSOPS.DemoESR.MobileSVC/",
+	//"SharePointRootUrl" : "https://intranet.tams.com/"
 	
 	
-	//"ServiceRootUrl": "http://tusspdev1wfe1/VirtualApps/TAMS.BUSOPS/TAMS.BUSOPS.DemoESR.MobileSVC/",
-	//"SharePointRootUrl" : "http://tusspdev1wfe1/"
+	"ServiceRootUrl": "http://tusspdev1wfe1/VirtualApps/TAMS.BUSOPS/TAMS.BUSOPS.DemoESR.MobileSVC/",
+	"SharePointRootUrl" : "http://tusspdev1wfe1/"
 	
 	
 	
