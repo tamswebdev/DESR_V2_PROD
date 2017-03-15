0a34665166c10a87529e5542291c1ea5f8436aae
tamswebdev <portaladmin@tams.com>
2017-03-15 09:45:52 -0700

    with environment-configs updated to use tusspdev1wfe3

--- Scripts/environment-configs.js
+++ Scripts/environment-configs.js
@@ -1,18 +1,15 @@
 //This is a configuration file for all hard-coded values in a specific environment
 
 var Configs = {
-	"ServiceRootUrl": "https://intranet.tams.com/VirtualApps/TAMS.BUSOPS/BUSOPSWebs/TAMS.BUSOPS.DemoESR.MobileSVC/",
-	"SharePointRootUrl" : "https://intranet.tams.com/",
-	"SitePath" : "sites/busops", "MKTSitePath" : "sites/marketing"
+	//"ServiceRootUrl": "https://intranet.tams.com/VirtualApps/TAMS.BUSOPS/BUSOPSWebs/TAMS.BUSOPS.DemoESR.MobileSVC/",
+	//"SharePointRootUrl" : "https://intranet.tams.com/",
+	//"SitePath" : "sites/busops", "MKTSitePath" : "sites/marketing"
 
 	
 	
-	//"ServiceRootUrl": "http://tusspdev1wfe1/VirtualApps/TAMS.BUSOPS/TAMS.BUSOPS.DemoESR.MobileSVC/",
-	//"SharePointRootUrl" : "http://tusspdev1wfe1/",
-	//"SitePath" : "sites/busops", "MKTSitePath" : "sites/marketing"
-	
-	
-	
+    "ServiceRootUrl": "http://tusspdev1wfe3/VirtualApps/TAMS.BUSOPS/TAMS.BUSOPS.DemoESR.MobileSVC/",
+    "SharePointRootUrl": "http://tusspdev1wfe3/",
+	"SitePath" : "sites/busops", "MKTSitePath" : "sites/marketing"	
 		
 	
 };
