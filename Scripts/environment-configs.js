e3b0979f1430c90142a5e0f90f6fc2921092d260
kho <kho@tams.com>
2017-05-04 16:07:17 -0700

    1

--- Scripts/environment-configs.js
+++ Scripts/environment-configs.js
@@ -2,7 +2,7 @@
 
 //This AppVersion must be matched with the version in API.
 //Otherwise, the app will force users to update
-var AppVersion = "5.4.6";
+var AppVersion = "5.4.7";
 
 var Configs = {
 	//"ServiceRootUrl": "https://intranet.tams.com/VirtualApps/TAMS.BUSOPS/BUSOPSWebs/TAMS.BUSOPS.DemoESR.MobileSVC/",
@@ -11,16 +11,16 @@ var Configs = {
 
 	
 	
-	"ServiceRootUrl": "http://tusspdev1wfe4/VirtualApps/TAMS.BUSOPS/TAMS.BUSOPS.DemoESR.MobileSVC/",
-	"SharePointRootUrl" : "http://tusspdev1wfe4/",
-	"EquipmentSitePath": "sites/busops/Logistics/DER",
-	"SitePath": "sites/busops",
-    "MKTSitePath": "sites/marketing"		
+	//"ServiceRootUrl": "http://tusspdev1wfe4/VirtualApps/TAMS.BUSOPS/TAMS.BUSOPS.DemoESR.MobileSVC/",
+	//"SharePointRootUrl" : "http://tusspdev1wfe4/",
+	//"EquipmentSitePath": "sites/busops/Logistics/DER",
+	//"SitePath": "sites/busops",
+    //"MKTSitePath": "sites/marketing"		
 
-    //"ServiceRootUrl": "https://intranetqa.tams.com/VirtualApps/TAMS.BUSOPS/BUSOPSWebs/TAMS.BUSOPS.DemoESR.MobileSVC/",
-    //"SharePointRootUrl": "https://intranetqa.tams.com/",
-    //"EquipmentSitePath": "sites/busops/Logistics/DER",
-    //"SitePath": "sites/busops",
-    //"MKTSitePath": "sites/marketing"
+    "ServiceRootUrl": "https://intranetqa.tams.com/VirtualApps/TAMS.BUSOPS/BUSOPSWebs/TAMS.BUSOPS.DemoESR.MobileSVC/",
+    "SharePointRootUrl": "https://intranetqa.tams.com/",
+    "EquipmentSitePath": "sites/busops/Logistics/DER",
+    "SitePath": "sites/busops",
+    "MKTSitePath": "sites/marketing"
 	
 };
